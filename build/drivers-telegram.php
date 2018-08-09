79d7d8ecad3a00b733e31c86c2998d6af795431f
Vladimir Yuldashev <misterio92@gmail.com>
2018-08-10 00:59:31 +0300

    wip

--- build/drivers-telegram.php
+++ build/drivers-telegram.php
@@ -8,20 +8,18 @@ use Symfony\Component\Finder\Finder;
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in($dir = '../../drivers-telegram/src')
-;
+    ->in($dir = __DIR__ . '/repositories/drivers-telegram/src');
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
-    ->add('master');
-;
+    ->add('master');;
 
 return new Sami($iterator, array(
-    'theme'                => 'default',
-    'versions'             => $versions,
-    'title'                => 'FondBot: Telegram Driver API',
-    'build_dir'            => __DIR__.'/build/drivers-telegram/%version%',
-    'cache_dir'            => __DIR__.'/cache/drivers-telegram/%version%',
-    'remote_repository'    => new GitHubRemoteRepository('fondbot/drivers-telegram', dirname($dir)),
+    'theme' => 'default',
+    'versions' => $versions,
+    'title' => 'FondBot: Telegram Driver API',
+    'build_dir' => __DIR__ . '/build/drivers-telegram/%version%',
+    'cache_dir' => __DIR__ . '/cache/drivers-telegram/%version%',
+    'remote_repository' => new GitHubRemoteRepository('fondbot/drivers-telegram', dirname($dir)),
     'default_opened_level' => 2,
 ));
--- build/framework.php
+++ build/framework.php
@@ -8,21 +8,19 @@ use Symfony\Component\Finder\Finder;
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in($dir = '../../framework/src')
-;
+    ->in($dir = __DIR__ . '/repositories/framework/src');
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
     ->add('3.0', 'v3.0')
-    ->add('master', 'v3.1-dev');
-;
+    ->add('master', 'v3.1-dev');;
 
 return new Sami($iterator, array(
-    'theme'                => 'default',
-    'versions'             => $versions,
-    'title'                => 'FondBot API',
-    'build_dir'            => __DIR__.'/build/framework/%version%',
-    'cache_dir'            => __DIR__.'/cache/framework/%version%',
-    'remote_repository'    => new GitHubRemoteRepository('fondbot/framework', dirname($dir)),
+    'theme' => 'default',
+    'versions' => $versions,
+    'title' => 'FondBot API',
+    'build_dir' => __DIR__ . '/build/framework/%version%',
+    'cache_dir' => __DIR__ . '/cache/framework/%version%',
+    'remote_repository' => new GitHubRemoteRepository('fondbot/framework', dirname($dir)),
     'default_opened_level' => 2,
 ));
